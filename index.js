3749d8990101b4aa8ca52412d898adea4bf532db
Eva <yi.zheng1709@gmail.com>
2021-04-04 22:26:12 -0400

    commit tree 1e328f878df9817b958ced699d4d5eaeddc3548e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
+
+function countDown(number) {
+    let i = number
+    while (i > -1) {
+        console.log(i)
+        i --
+    }
+}
